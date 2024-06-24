42f92fb08223118958c3b73ebfc8598049e719fc
soumya720 <57815662+soumya720@users.noreply.github.com>
2024-06-24 05:15:45 +0000

    my first program

---
+++ .devcontainer/01_basics/01_variable.js
@@ -0,0 +1,10 @@
+const accountId = 144553
+let accountEmail = "hitesh@google.com"
+var accountPassword = "12345"
+accountCity = "jaipur"
+//accountId = 2
+console.log(accountId);
+accountEmail = "hc@hc.com"
+accountPassword = "21212121"
+accountCity = "Bengaluru"
+co
